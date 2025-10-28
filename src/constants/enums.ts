a1a5a4c8378499c0902ffe0522f34798bff65a28
Marcelo Ramos <marcelorcramos@gmail.com>
2025-10-28 12:51:10 +0000

    Refactor and expand enums for consistency and clarity
    Updated enums to use lowercase values and improved label translations. Replaced Department enum key 'AD' with 'ADMINISTRACAO' for clarity. Added new enums and options for TaskStatus and UserType, providing consistent structure and labels for each. Refactored options arrays to explicit lists for better maintainability.

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,51 +1,69 @@
 // Enum NivelExperiencia
 export enum NivelExperiencia {
-    JUNIOR = "Junior",
-    SENIOR = "Senior"
-};
-
-export const NivelExperienciaLabels = {
+    JUNIOR = "junior",
+    SENIOR = "senior"
+  }
+  
+  export const NivelExperienciaLabels = {
     [NivelExperiencia.JUNIOR]: 'Junior',
-    [NivelExperiencia.SENIOR]: 'Senior'
-};
-
-export const NivelExperienciaOptions = Object.values(NivelExperiencia).map(dept => ({
-    value: dept,
-    label: NivelExperienciaLabels[dept]
-})); 
-
-// Enum Departamento
-export enum Department {
+    [NivelExperiencia.SENIOR]: 'Sénior'
+  };
+  
+  export const NivelExperienciaOptions = [
+    { value: NivelExperiencia.JUNIOR, label: NivelExperienciaLabels[NivelExperiencia.JUNIOR] },
+    { value: NivelExperiencia.SENIOR, label: NivelExperienciaLabels[NivelExperiencia.SENIOR] }
+  ];
+  
+  // Enum Departamento
+  export enum Department {
     IT = "IT",
-    AD = "Administração",
-    MARKETING = "Marketing",
-}
-
-export const DepartmentLabels = {
+    ADMINISTRACAO = "Administração",
+    MARKETING = "Marketing"
+  }
+  
+  export const DepartmentLabels = {
     [Department.IT]: 'Tecnologia da Informação',
-    [Department.AD]: 'Administração',
+    [Department.ADMINISTRACAO]: 'Administração', 
     [Department.MARKETING]: 'Marketing'
-};
-
-export const DepartmentOptions = Object.values(Department).map(dept => ({
-    value: dept,
-    label: DepartmentLabels[dept]
-}));
-
-// Enum EstadoAtual
-export enum EstadoAtual {
-    TODO = "To Do",
-    INPROGRESS = "In progress",
-    DONE = "Done"
-};
-
-export const EstadoAtualLabels = {
-    [EstadoAtual.TODO]: 'Tod do',
-    [EstadoAtual.INPROGRESS]: 'In progress',
-    [EstadoAtual.DONE]: 'Done'
-};
-
-export const EstadoAtualOptions = Object.values(EstadoAtual).map(dept => ({
-    value: dept,
-    label: EstadoAtualLabels[dept]
-}));
+  };
+  
+  export const DepartmentOptions = [
+    { value: Department.IT, label: DepartmentLabels[Department.IT] },
+    { value: Department.ADMINISTRACAO, label: DepartmentLabels[Department.ADMINISTRACAO] },
+    { value: Department.MARKETING, label: DepartmentLabels[Department.MARKETING] }
+  ];
+  
+  // Enum EstadoAtual (para tarefas)
+  export enum TaskStatus {
+    TODO = "todo",
+    DOING = "doing", 
+    DONE = "done"
+  }
+  
+  export const TaskStatusLabels = {
+    [TaskStatus.TODO]: 'A Fazer',
+    [TaskStatus.DOING]: 'Em Progresso',
+    [TaskStatus.DONE]: 'Concluído'
+  };
+  
+  export const TaskStatusOptions = [
+    { value: TaskStatus.TODO, label: TaskStatusLabels[TaskStatus.TODO] },
+    { value: TaskStatus.DOING, label: TaskStatusLabels[TaskStatus.DOING] },
+    { value: TaskStatus.DONE, label: TaskStatusLabels[TaskStatus.DONE] }
+  ];
+  
+  // Enum UserType
+  export enum UserType {
+    MANAGER = "manager",
+    PROGRAMMER = "programmer"
+  }
+  
+  export const UserTypeLabels = {
+    [UserType.MANAGER]: 'Gestor',
+    [UserType.PROGRAMMER]: 'Programador'
+  };
+  
+  export const UserTypeOptions = [
+    { value: UserType.MANAGER, label: UserTypeLabels[UserType.MANAGER] },
+    { value: UserType.PROGRAMMER, label: UserTypeLabels[UserType.PROGRAMMER] }
+  ];
